bc21cffd282cb36867f4f46160d0d595f9ba2215
Marilyn Niere <marilynniere716@gmail.com>
2024-10-16 13:27:04 +0800

    Add files via upload

---
+++ Prelim/Activity IV/NIERE_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class NIERE_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int sale1 = 243;
+        int sale2 = 897;
+        int sale3 = 834;
+
+        int sum = sale1 + sale3;
+        int diff = sale2 -sale3;
+        int prod = sale3 * sale1;
+        int quot = sale2 /sale2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
