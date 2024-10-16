78d0bb95e4a6e20eb9876fa23b8495db5134132c
Kianoooooo <mariobaruel13@gmail.com>
2024-10-16 13:49:29 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAPARIDA_ACTIVITY.IV.java
@@ -0,0 +1,18 @@
+class CAPARIDA_ACTIVITY.IV {
+    Run|Debug 
+    public static void main(string[] args){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b;
+        int diff = a - b;
+        int prod = c * a;
+        int quat = b / c;
+
+        system.out.printIn(x:"result");
+        system.out.printIn(x:"amount");
+        system.out.printIn(x:"total");
+        system.out.printIn(x:"height");        
+    }    
+}
