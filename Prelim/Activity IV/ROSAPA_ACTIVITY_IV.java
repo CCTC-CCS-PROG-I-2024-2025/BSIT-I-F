a742a54d1569dd18593f5376337f602cb06153a7
Cherry Mae Rosapa <cherrymaerosapa3@gmail.com>
2024-10-16 12:30:15 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ROSAPA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class ROSAPA_ACTIVITY_IV {
+  public static void main(String[] args) {
+    int x = 15;
+    int y = 5;
+    int z = 3;
+
+    int sum = x + y + z;
+    int diff = x - z;
+    int prod = x * y;
+    int qout = x / y;
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(qout);
+  }
+}
