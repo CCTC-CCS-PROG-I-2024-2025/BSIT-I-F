5e9e0c893d61d0eeb0132b399e9134440df18a7c
Melli Aileen D. Heraldez <heraldezmelli7@gmail.com>
2024-10-16 12:53:45 +0800

    Add files via upload

--- Prelim/Activity IV/HERALDEZ_ACTIVITY_IV.java
+++ Prelim/Activity IV/HERALDEZ_ACTIVITY_IV.java
@@ -1,22 +1,17 @@
 public class HERALDEZ_ACTIVITY_IV {
-        public static void main(String[] args) {
-    //VARIABLE
-            int x = 15;
-            int y = 10;
-            int c = 9;
-    //ADDITION
-            int result = x + y;
-    //SUBTRACTION
-            int result1 = y - x;
-    //DIVIDE
-            int result2 = x / c;
-    //MULTIPLICATION
-            int result3 = c * y;
-    //PRINTING
-            System.out.println(result);
-            System.out.println(result1);
-            System.out.println(result2);
-            System.out.println(result3);
-                }
-    }
+    public static void main(String[] args) {
+        int sales1 = 243;
+        int sales2 = 897;
+        int sales3 = 834;
 
+        int sum = sales1 + sales3;
+        int diff = sales2 - sales3;
+        int prod = sales3 * sales1;
+        int qout = sales2 / sales2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qout);
+    }
+}
