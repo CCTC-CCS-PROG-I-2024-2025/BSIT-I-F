12ce8c845c6347bd9655bf7fe678cf88bef50f15
Byrhoncast <castbyrhon@gmail.com>
2024-10-16 13:41:03 +0800

    Add files via upload

---
+++ Prelim/Activity I/CASTOLO_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+/**
+ * CASTOLO_ACTIVITY_1
+ */
+public class CASTOLO_ACTIVITY_I {
+public static void main(String[] args) {
+    System.out.println("Byrhon P. Castolo");
+    System.out.println(19);
+    System.out.println(18);
+    System.out.println(19+18);
+}
+
+    
+}
