db077200f80d557ccd01a6a142af8078309ec3e6
Clifford briones <cliffordbriones6@gmail.com>
2024-10-16 14:03:34 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BRIONES_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class BRIONES_ACTIVITY_IV.java{
+    public static void main(String[]args){
+        int close = 90;
+        int me = 1;
+        int students = 10;
+
+        int sum = close+students;
+        int amount = me-close;
+        int population = students*close;
+        int total = me / students;
+        
+        System.out.println(sum);
+        System.out.println(amount);
+        System.out.println(population);
+        System.out.println(total);
+    }
+}
