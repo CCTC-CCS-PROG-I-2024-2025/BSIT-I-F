16c2e56e65cf258146857aeae613577f63372a29
Tristan Dreco Garcia <garciatristan66@gmail.com>
2024-10-16 12:34:20 +0800

    Add files via upload

---
+++ Prelim/Activity I/GARCIA_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class GARCIA_ACTIVITY_I {
+    public static void main (String[] args) {
+        System.out.println("Tristan Garcia");
+        System.out.println("18");
+        System.out.println("21");
+        System.out.println(18+21);
+    }
+}
