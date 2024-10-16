485b8b5b8fa51ef4d1fe7455d0e480acffee3c92
Dave O. Prajes <daveprajes0@gmail.com>
2024-10-16 13:33:39 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PRAJES_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+class PRAJES_ACTIVITY_IV {
+    public static void main (String[] args){
+        int x=30;
+        int y=40;
+        int q=13;
+        int total=x + y;
+        int amount=y - q;
+        int money=q * x;
+        int height=x / y;
+        System.out.println(total);
+        System.out.println(amount);
+        System.out.println(money);
+        System.out.println(height);
+    }
+}
