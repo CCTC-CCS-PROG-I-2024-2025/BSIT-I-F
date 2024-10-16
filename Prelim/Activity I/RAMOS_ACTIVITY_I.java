dfe79420c60b7e6a08eb8c187998c7fd6265a010
Minnie Grace D. Ramos <ramosminniegrace@gmail.com>
2024-10-16 12:31:00 +0800

    Add files via upload

---
+++ Prelim/Activity I/RAMOS_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class RAMOS_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Minnie Grace D. Ramos");
+        System.out.println(27);
+        System.out.println(20);
+        System.out.println(27+20);
+    }
+}
