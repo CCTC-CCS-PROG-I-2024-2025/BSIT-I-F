407583d25493d84f8a48c4c12a9eadcd3f641599
Debbie Mariz Gilhang <marizgilhang104@gmail.com>
2024-10-16 12:32:19 +0800

    Add files via upload

---
+++ Prelim/Activity I/GILHANG_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class GILHANG_ACTIVITY_I{
+    public static void main(String[] args) {
+        System.out.println("Mariz Gilhang");
+        System.out.println(17);
+        System.out.println(19);
+        System.out.println(17+19);
+    }
+}
