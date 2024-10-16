eba603517e66162e89257b88d556b3bbb530345b
Ciara Dawn A. Tan <ciaradawntan12@gmail.com>
2024-10-16 12:32:25 +0800

    Add files via upload

---
+++ Prelim/Activity I/TAN_ACTIVITY_I.java
@@ -0,0 +1,7 @@
+class TAN_ACTIVITY_I {
+    public static void main (String[] angs) {
+        System.out.println("Ciara Dawn Tan");
+        System.out.println(19); 
+        System.out.println(17);
+        System.out.println(19+17); }
+        }
