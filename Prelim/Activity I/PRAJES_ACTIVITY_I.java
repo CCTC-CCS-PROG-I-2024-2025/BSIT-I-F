6e326f804ac904e75204f86b396f42d134b85706
Dave O. Prajes <daveprajes0@gmail.com>
2024-10-16 12:33:16 +0800

    Add files via upload

---
+++ Prelim/Activity I/PRAJES_ACTIVITY_I.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class PRAJES_ACTIVITY_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Color");
+        String color = sc.nextLine();
+
+        switch (color){
+            case "red":
+               System.out.println(1);
+               break;
+            case "green":
+               System.out.println(2);
+               break;
+            case "black":
+               System.out.println(3);
+               break;
+        
+        
+        }
+    }
+}
+
+
+
+                 
