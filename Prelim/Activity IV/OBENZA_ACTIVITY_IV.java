e5f293910e8f319a22653aa113093efe802883ba
Christian Obenza <obenzachristian15@gmail.com>
2024-10-16 12:35:58 +0800

    Add files via upload

---
+++ Prelim/Activity IV/OBENZA_ACTIVITY_IV.java
@@ -0,0 +1,23 @@
+public class OBENZA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age=24;
+        int number=20;
+        int price=67;
+        int result=age + number
+        int amount=number - price;
+        int total=price * number;
+        int height=number / price;
+        System.out.println(result);
+        System.out.println(amount);
+        System.out.println(total);
+        System.out.println(height);
+        
+
+
+        
+
+
+    }
+}
+
+    
