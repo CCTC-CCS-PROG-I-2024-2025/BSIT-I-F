c5b8521a97e9852b1756a9bdef81bb95b600fc84
Aljun B. Flores <aljunflores373@gmail.com>
2024-10-16 12:31:21 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FLORES_ACTIVITY_IV.java
@@ -0,0 +1,28 @@
+class FLORES_ACTIVITY_I {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = 10 + 5;
+
+        int a1 = 10;
+        int b1 = 5;
+        int result1 = a1 - b1;
+
+        int a2 = 10;
+        int b2 = 5;
+        int result2 = a2 / b2;
+
+        int a3 = 10;
+        int b3 = 5;
+        int result3 = a3 * b3;
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+
+}
+
+    
+
