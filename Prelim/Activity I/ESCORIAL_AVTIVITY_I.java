74c0fe6ef716730ddcf172b7a878301ca0f95e20
Ana Lea Escorial <analeatangoan08@gmail.com>
2024-10-16 12:31:52 +0800

    Add files via upload

---
+++ Prelim/Activity I/ESCORIAL_AVTIVITY_I.java
@@ -0,0 +1,9 @@
+class ESCORIAL_ACTIVITY_I { 
+    
+public static void main(String[] args) {
+        System.out.println("ana lea escorial");
+        System.out.println(20);
+        System.out.println(18);
+        System.out.println(20+18 );
+    }
+}
