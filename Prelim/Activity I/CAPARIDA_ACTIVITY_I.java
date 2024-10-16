306978167dae52dc10df0fad7436519e412abb17
Kianoooooo <mariobaruel13@gmail.com>
2024-10-16 13:49:08 +0800

    Add files via upload

---
+++ Prelim/Activity I/CAPARIDA_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class CAPARIDA_ACTIVITY_I {
+    Run|Debug
+    public static voin main (String[] args) {
+    System.out.printin(KIAN CAPARIDA);
+    System.out.printin(x:23);
+    System.out.printin(x:20); 
+    System.out.printin(23+20);   
+}
