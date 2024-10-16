1d3614121c57ad289f9a6838020a0114a44464cb
Izza May Derder <izamd4710@gmail.com>
2024-10-16 12:33:00 +0800

    Add files via upload

---
+++ Prelim/Activity I/DERDER_ACTIVITY_I.java
@@ -0,0 +1,7 @@
+class DERDER_ACTIVITY_I {
+    public static void main(String[] angs) {
+        System.out.println("Izza May Derder");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18+18); }
+    }
