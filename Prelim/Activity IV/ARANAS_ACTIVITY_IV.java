1c08c1c85849bd56c7bc4a0428ac3d68ebbc6157
amoraelyse02 <luisenatasha02@gmail.com>
2024-10-16 12:36:28 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ARANAS_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+class ARANAS_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 55;
+        int c = 55;
+        int d = 90;
+        int b = a + d;
+        int e = c - a;
+        int x = d * a;
+        int y = d / c;
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+    }
+}
