47747e607be6c96f130135380f5d11fedc5dcba5
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2024-10-16 12:39:25 +0800

    Add files via upload

---
+++ Prelim/Activity IV/HERALDEZ_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+public class HERALDEZ_ACTIVITY_IV {
+        public static void main(String[] args) {
+    //VARIABLE
+            int x = 15;
+            int y = 10;
+            int c = 9;
+    //ADDITION
+            int result = x + y;
+    //SUBTRACTION
+            int result1 = y - x;
+    //DIVIDE
+            int result2 = x / c;
+    //MULTIPLICATION
+            int result3 = c * y;
+    //PRINTING
+            System.out.println(result);
+            System.out.println(result1);
+            System.out.println(result2);
+            System.out.println(result3);
+                }
+    }
+
