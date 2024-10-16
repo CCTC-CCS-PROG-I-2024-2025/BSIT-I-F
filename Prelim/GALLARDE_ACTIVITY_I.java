ba1c329fba689966ef4da477d5a98b31647ddf1f
Shiella Gallarde <gallardeshiella@gmail.com>
2024-10-16 12:38:46 +0800

    Add files via upload

---
+++ Prelim/GALLARDE_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+class GALLARDE_ACTIVITY_IV {
+    public static void main (String[] args) {
+        System.out.println("Shiella Gallarde");
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20+20);
+
+    }
+}
