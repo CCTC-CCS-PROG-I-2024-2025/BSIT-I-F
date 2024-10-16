29441ff0c790a0c04bea5b5e4bf76de867dcdb4d
Christian M. Ubas <bewakerchristian@gmail.com>
2024-10-16 13:28:01 +0800

    Add files via upload

---
+++ Prelim/Activity I/UBAS_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class UBAS_ACTIVITY_I {
+    public static void main (String[] args) {
+        System.out.println("Ubas Christian M");
+        System.out.println("19");
+        System.out.println("18");
+        System.out.println("19+18");
+    }
+}
