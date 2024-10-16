c70bdf0f0a1ff350f777e4d26245f6afe14b44f9
Clifford briones <cliffordbriones6@gmail.com>
2024-10-16 14:02:28 +0800

    Add files via upload

---
+++ Prelim/Activity I/BRIONES_ACTICITY_I.java
@@ -0,0 +1,9 @@
+class BRIONES_ACTICITY_I {
+    public static void main (string[] args) {
+        System.out.println("Clifford Briones"); 
+        System.out.println(19);
+        System.out.println(18);
+        System.out.println(19+18);
+    }
+    
+}
