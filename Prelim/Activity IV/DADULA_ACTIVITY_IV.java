3de54fd187a06d13509c1ad9b20df469dd5e7dbd
Jessa Mae R. Dadula <samyatsang412@gmail.com>
2024-10-16 12:33:02 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DADULA_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class DADULA_ACTIVITY_I {
+    public static void main(String[] args){
+        int x = 12;
+        int y = 4;
+        int w = 6;
+
+        int sum = x + y + w;
+        int diff = x - y;
+        int prod = x * y;
+        int quo = x / y;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quo);
+    }
+}
