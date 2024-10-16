3ce0d3eb4d79d187b91310c1fea7b76a16996cb5
Minnie Grace D. Ramos <ramosminniegrace@gmail.com>
2024-10-16 12:35:08 +0800

    Add files via upload

---
+++ Prelim/Activity IV/RAMOS_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+class RAMOS_ACTIVITY_IV {
+    public static void main(String[] args) {
+
+       int a = 100;
+       int b = 200;
+       int c = 450;
+
+       int sum = a + b;
+       int diff = b - c;
+       int prod = c * a;
+       int quot = b / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+}
