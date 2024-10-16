e3729b70e871a9a6b4f92d05af67a6bfe35f0572
Melli Aileen D. Heraldez <heraldezmelli7@gmail.com>
2024-10-16 13:41:01 +0800

    Add files via upload

--- Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
+++ Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,8 +1,8 @@
-class HERALDEZ_ACTIVITY_I{
-    public static void main (String[] args) {
-        System.out.println("KURT JOHN HERALDEZ");
-        System.out.println(20);
+class HERALDEZ_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Melli Aileen Heraldez");
+        System.out.println(23);
         System.out.println(18);
-        System.out.println(20+18);
-    }
-}
+        System.out.println(23+18);
+    }        
+}   
