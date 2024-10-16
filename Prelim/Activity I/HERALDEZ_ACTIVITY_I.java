9300efa422565087b19441687f770396befd4a2b
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2024-10-16 13:31:02 +0800

    Update HERALDEZ_ACTIVITY_I.java

--- Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
+++ Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,12 +1,8 @@
 class HERALDEZ_ACTIVITY_I{
     public static void main (String[] args) {
-        //Name
         System.out.println("KURT JOHN HERALDEZ");
-        //My age
         System.out.println(20);
-        //Seatmate's age
         System.out.println(18);
-        //Sum of our age
         System.out.println(20+18);
     }
 }
