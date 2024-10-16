fdeffe5e64b2f431e18430a444de6dc255700539
Sphere Alqueza <spherealqueza5@gmail.com>
2024-10-16 14:01:09 +0800

    Delete Prelim/Activity I/ALQUEZA_ACTIVITY_I.java

--- Prelim/Activity I/ALQUEZA_ACTIVITY_I.java
+++
@@ -1,9 +0,0 @@
-class ALQUEZA_ACTIVITY_I {
-    Run|Debug
-    public static void main (String[] args){
-        System.out.printIn(SPHERE ALQUEZA);
-        System.out.printIn(x:20);
-        System.out,printIn(x:23);
-        System.out,printIn(20+23);
-    }
-}
