948255b328a1f7dabc5d2ffb9d96192578be41dc
Aljun B. Flores <aljunflores373@gmail.com>
2024-10-16 12:34:23 +0800

    Delete Prelim/FLORES_ACTIVITY_I.java

--- Prelim/FLORES_ACTIVITY_I.java
+++
@@ -1,9 +0,0 @@
-class FLORES_ACTIVITY_I {
-public static void main (String[] args) {
-    System.out.println("Aljun B. Flores");
-    System.out.println(19);
-    System.out.println(18);
-    System.out.println(19+18); }
-}
-
-
