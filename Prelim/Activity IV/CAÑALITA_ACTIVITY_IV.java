04c31c60fff235580bed3cf71743cbe1d487b8c7
carlitojrfajardo <carlitojrfajardo@gmail.com>
2024-10-16 12:40:22 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CAÑALITA_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+public class CAÑALITA_ACTIVITY_IV {
+    public static void main(String[]args) {
+       int a = 10;
+       int b = 20;
+       int c = 30;
+       
+
+       int sum = a+b;
+       int diff = b-c;
+       int prod = c*a;
+    }  int qout = b/c;
+       
+      System.out.println(sum);
+      System.out.println(diff);
+      System.out.println(prod);
+      System.out.println(qout);
+
+    
+}
