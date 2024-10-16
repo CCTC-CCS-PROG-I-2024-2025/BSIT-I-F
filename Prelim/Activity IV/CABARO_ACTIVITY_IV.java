ed5fe4e72e8b58cf8fd9e6db901e58becad11156
Rafael Cabardo <rafaelcabardo24@gmail.com>
2024-10-16 12:40:52 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CABARO_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+class CABARDO_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int y = 35;
+        int x = 42;
+        int c = 80;
+        int a = x + y;
+        int b = x - c;
+        int d = c * y;
+        int e = x / c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(e);
+
+
+
+
+
+
+    }
+}
