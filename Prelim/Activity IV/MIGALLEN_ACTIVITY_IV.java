b3aef566750e316b0fa7d9203b222a9f0a1822ab
Feil Migallen <migallenfeil005@gmail.com>
2024-10-16 12:30:09 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MIGALLEN_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+public class MIGALLEN_ACTIVITY_IV {
+      public static void main (String[] args) {   
+          int z = 5000; 
+          int y = 50; 
+          int result = z + y; 
+
+          int z1 = 5000; 
+          int y1 = 50; 
+          int result1 = z1 - y1; 
+          
+          int z2 = 5000;
+          int y2 = 50; 
+          int result2 = z2 / y2; 
+          
+          int z3 = 5000; 
+          int y3 = 50; 
+          int result3 = z3 * y3; 
+
+          System.out.println(result); 
+          System.out.println(result1); 
+          System.out.println(result2); 
+          System.out.println(result3);
+          
+      }
+}
