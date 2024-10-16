877bbd29d3e4addaf79e666cc5d7f80fa9e8da30
Julienne J. Layon <layonjuliennejakosalem@gmail.com>
2024-10-16 12:27:16 +0800

    Add files via upload

---
+++ Prelim/Activity I/LAYON_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class LAYON_ACTIVITY_I {
+  public static void main(String[] args) {
+    System.out.println("Julienne J. Layon");
+    System.out.println(20);
+    System.out.println(20);
+    System.out.println(20 + 20);
+  }
+}
