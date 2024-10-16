9068fff93fceee1257b5fcbe312c0a450f96c9f5
Kym Jeo Minamen <kymjeom@gmail.com>
2024-10-16 12:41:19 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MINAMEN_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+public class MINAMEN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age = 23;   
+        int number = 20;
+        int price = 15;
+        int result = age + number;
+        int amount = number - price; 
+        int total = price * number;
+        int height = number / price;
+        System.out.println(result);
+        System.out.println(amount);
+        System.out.println(total);
+        System.out.println(height);
+
+
+
+    
+    }
+}
