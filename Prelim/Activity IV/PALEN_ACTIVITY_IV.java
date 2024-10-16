df0386e3674a6554b19b9a8ee7318a91eae267db
Herzel Palen <palenherzel05@gmail.com>
2024-10-16 12:29:30 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PALEN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+public class PALEN_ACTIVITY_IV { 
+    public static void main(String[] args) {
+        int sales1 = 243;
+        int sales2 = 897;
+        int sales3 = 834;
+
+        int sum = sales1 + sales3;
+        int diff = sales2 - sales3;
+        int prod = sales3 * sales1;
+        int qout = sales2 / sales2;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(qout);
+    }
+}
