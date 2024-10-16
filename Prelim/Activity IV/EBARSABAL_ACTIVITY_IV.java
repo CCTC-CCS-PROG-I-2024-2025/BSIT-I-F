48c722a3e544ec30f1d90913b6909062569d80b1
Rhea Liera Binondo Ebarsabal <dinoshannen@gmail.com>
2024-10-16 12:32:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/EBARSABAL_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+class EBARSABAL_ACTIVITY_IV {
+    public static void main(String[] args) {
+    int a=30;
+    int b=20;
+    int c=10;
+
+    int sum=(a+b);
+    int diff=(b-c);
+    int prod=(a*c);
+    int qout=(a/c);
+
+    System.out.println(sum);
+    System.out.println(diff);
+    System.out.println(prod);
+    System.out.println(qout);
+    
+      }
+}
