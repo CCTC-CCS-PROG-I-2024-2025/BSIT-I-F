51a5d275a9660f89c62f05ddbb0d41c9e8569416
Shiella Gallarde <gallardeshiella@gmail.com>
2024-10-16 12:42:32 +0800

    Add files via upload

---
+++ Prelim/GALLARDE_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+class GALLARDE_ACTIVITY_IV {
+    public static void main (String[] args) {
+        int a = 80;
+        int c = 24;
+        int d = 5;
+        int b = a + d;
+        int e = c - a;
+        int x = d * a;
+        int y = d / c;
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+    }
+}
