51403d88d8a8e9d237960dc98246e00d19cdfc49
John Rian Cabase <johnriancabase1@gmail.com>
2024-10-16 12:28:39 +0800

    Add files via upload

---
+++ Prelim/Activity I/CABASE_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+public class CABASE_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("JOHN RIAN CABASE");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18+18);
+
+    }
+}
