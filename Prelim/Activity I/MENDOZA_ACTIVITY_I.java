cf5cc34a6cb6574187808c5f22b3df2cda3b9a36
Charisse Mae Mendoza <charissemendoza194@gmail.com>
2024-10-16 12:31:26 +0800

    Add files via upload

---
+++ Prelim/Activity I/MENDOZA_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class MENDOZA_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Charisse Mae Mendoza");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18+19);
+    }
+}
