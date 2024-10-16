4e92184266e947f4840f65f066f35a07c2a43720
Dofe Borres <kyriedofe@gmail.com>
2024-10-16 12:34:39 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BORRES_ACTIVITY_IV.java
@@ -0,0 +1,9 @@
+ class BORRES_ACTIVITY_VI {
+    public static void main(String[] args) {
+       Scanner sc = neww scanner(System.in);
+       Stringname = sc . nextLine();
+       int age = sc . nextLine();
+       System.out.println("Welcome:"+name);
+       System.out.println("Age:"+ age);
+    }
+} 
