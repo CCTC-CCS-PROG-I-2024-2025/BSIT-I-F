38ddaa8bf9c0f09648dd936c1962bec5693219a8
Ciara Dawn A. Tan <ciaradawntan12@gmail.com>
2024-10-16 12:34:58 +0800

    Add files via upload

---
+++ Prelim/Activity IV/TAN_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+class TAN_ACTIVITY_IV {
+     public static void main(String[] args) {
+        int a = 12;
+        int b = 4;
+        int c = 2;
+
+        int sum = a + b + c;
+        int diff = a - b;
+        int prod = b * c;
+        int quot = a / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot); 
+        }
+} 
