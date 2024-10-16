aae42c152c2fbbe5a8b9e3689234ce4f539bbdc5
Christian Mark Doria <12doriachristian15@gmail.com>
2024-10-16 12:46:19 +0800

    Add files via upload

---
+++ Prelim/Activity I/DORIA_ACTIVITY_1.java
@@ -0,0 +1,14 @@
+ class DORIA_ACTIVITY_I {
+
+ 
+
+ public static void main(String[] args) {
+    System.out.println("Christian Doria");
+    System.out.println("18");
+    System.out.println("18");
+    System.out.println("18+18");
+    
+ }
+
+
+}
