e7f3984b1b4dc1eced741afd51b79467f39b7cac
Johannes Michael Mejia <ggxaragares@gmail.com>
2024-10-16 12:26:38 +0800

    Add files via upload

---
+++ Prelim/Activity I/MEJIA_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+class MEJIA_ACTIVITY_I{
+    public static void main (String[] args) {
+        //Name
+        System.out.println("Johannes Michael Mejia");
+        //My age
+        System.out.println(23);
+        //Seatmate's age
+        System.out.println(18);
+        //Sum of our age
+        System.out.println(23+18);
+    }
+}
