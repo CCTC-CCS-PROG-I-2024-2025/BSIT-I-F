6d100a38238480f948880612145cb83dca680357
Christian Obenza <obenzachristian15@gmail.com>
2024-10-16 12:30:23 +0800

    Add files via upload

---
+++ Prelim/Activity I/OBENZA_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class OBENZA_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Christian Obenza");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18+18);
+    }
+}
