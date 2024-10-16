6609cefbe520035048e5531ba4e2d7c90b7b8806
Kym Jeo Minamen <kymjeom@gmail.com>
2024-10-16 12:29:41 +0800

    Add files via upload

---
+++ Prelim/Activity I/MINAMEN_ACTIVITY_I.java
@@ -0,0 +1,10 @@
+class MINAMEN_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println( " Minamen Kym Jeo L");
+        System.out.println("18");
+        System.out.println("19");
+        System.out.println("18+19");
+  }
+}        
+    
+
