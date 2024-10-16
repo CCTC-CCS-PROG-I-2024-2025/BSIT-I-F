a70903bfc8a524c57187e5d44f34a7087f1a2ea3
Rafael Cabardo <rafaelcabardo24@gmail.com>
2024-10-16 12:40:29 +0800

    Add files via upload

---
+++ Prelim/Activity I/CABARDO_ACTIVITY_I.java
@@ -0,0 +1,10 @@
+ class CABARDO_ACTIVITY_I { 
+    public static void main(String[] args) {
+        System.out.println("Rafael cabardo");
+        System.out.println(18);
+        System.out.println(20);
+        System.out.println(18+20);
+        
+     
+    }
+ }
