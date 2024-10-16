cf9776f1871640fdaf1700e4270df8338a807344
Dofe Borres <kyriedofe@gmail.com>
2024-10-16 12:34:09 +0800

    Add files via upload

---
+++ Prelim/Activity I/BORRES_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class BORRES_ACTIVITY_I {
+    public static void main(String[] angs) {
+        System.out.println("Dofe Borres:");
+        System.out.println(19);
+        System.out.println(17);
+        System.out.println(19+17);
+    }
+}
