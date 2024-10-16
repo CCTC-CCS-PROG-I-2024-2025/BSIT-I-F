dbd1db2c2d824f7d0b94e3c74e3fd50fb9a3a6f8
Sphere Alqueza <spherealqueza5@gmail.com>
2024-10-16 14:01:34 +0800

    Add files via upload

---
+++ Prelim/Activity I/ALQUEZA_ACTIVITY_I.java
@@ -0,0 +1,12 @@
+ALQUEZA_ACTIVITY_I
+public class ALQUEZA_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        System.out.println(ALQUEZA SPHERE);
+        System.out.println(x:"20");
+        System.out.println(x:"23");
+        System.out.println(x:"43");
+        
+
+    }
+}
