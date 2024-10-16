8ac599d73b5e7a2f526a0dbd009fadc2942fe17a
Ana Lea Escorial <analeatangoan08@gmail.com>
2024-10-16 12:39:05 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ESCORIAL_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+class ESCORIAL_ACTIVITY_IV { 
+    public static void main(String[] args) {
+     int a = 52;  
+     int b = 46;
+     int c = 30;
+      
+     int sum = (a+b);
+     int diff = (b-c);
+     int prod = (b*c);
+     int quot = (a/c);
+
+      System.out.println(sum);
+      System.out.println(diff);
+      System.out.println(prod);
+      System.out.println(quot);
+      
+        }
+    }
