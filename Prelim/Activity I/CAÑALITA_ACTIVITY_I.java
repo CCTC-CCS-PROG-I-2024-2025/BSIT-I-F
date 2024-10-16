f42f567addd86b40bf4286607ed48f4e9216ebf8
carlitojrfajardo <carlitojrfajardo@gmail.com>
2024-10-16 12:33:02 +0800

    Add files via upload

---
+++ Prelim/Activity I/CAÑALITA_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+class CAÑALITA_ACTIVITY_I {
+    public static void main (String[] args) {
+        System.out.println ("Carlito Cañalita");
+        System.out.println(21);
+        System.out.println(19);
+        System.out.println(21+19);
+
+       }
+}
