16900e0ac64d50602f61b259cad35f424c396c2f
Largo,Timothy John O. <hoovyrickamaroni123@gmail.com>
2024-10-16 12:29:02 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LARGO_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class LARGO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        int c = 6;
+
+        int sum = a + b;
+        int diff= a - b;
+        int prod= a * c;
+        int quot= a / c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+    }
+    
+}
