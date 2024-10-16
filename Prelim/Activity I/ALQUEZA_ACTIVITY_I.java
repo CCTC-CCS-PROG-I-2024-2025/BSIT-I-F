0becf9deb9af6f9b4818e1610cf811f1dfcb0424
Sphere Alqueza <spherealqueza5@gmail.com>
2024-10-16 13:45:41 +0800

    Add files via upload

---
+++ Prelim/Activity I/ALQUEZA_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+class ALQUEZA_ACTIVITY_I {
+    Run|Debug
+    public static void main (String[] args){
+        System.out.printIn(SPHERE ALQUEZA);
+        System.out.printIn(x:20);
+        System.out,printIn(x:23);
+        System.out,printIn(20+23);
+    }
+}
