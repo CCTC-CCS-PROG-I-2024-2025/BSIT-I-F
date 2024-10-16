1bdd6520b7b3204ac88e2c8fc4f0d2fab830e994
Byrhoncast <castbyrhon@gmail.com>
2024-10-16 13:41:53 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CASTOLO_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+/**
+ * CASTOLO_ACTIVITY_IV
+ */
+public class CASTOLO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int value1 = 121;
+        int value2 = 681;
+        int value3 = 866;
+
+        int sum = value2 + value3;
+        int dif = value3 - value1;
+        int prod = value3 * value2;
+        int qout = value3 / value1;
+
+        System.out.println(sum);
+        System.out.println(dif);
+        System.out.println(prod);
+        System.out.println(qout);
+    }
+    
+}
