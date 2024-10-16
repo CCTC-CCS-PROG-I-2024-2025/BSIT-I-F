af7dff42b2753e3a74a72d0e58e6e57100d472b8
Julienne J. Layon <layonjuliennejakosalem@gmail.com>
2024-10-16 12:27:45 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LAYON_ACTIVITY_IV.java
@@ -0,0 +1,17 @@
+     class LAYON_ACTIVITY_IV {
+   public static void main(String[] args) {
+      int x = 12;
+      int y = 4;
+      int z = 6;
+
+      int sum = x + y + z;
+      int diff = x - z;
+      int prod = x * y;
+      int qout = x / y;
+
+      System.out.println(sum);
+      System.out.println(diff);
+      System.out.println(prod);
+      System.out.println(qout);
+   }
+}
