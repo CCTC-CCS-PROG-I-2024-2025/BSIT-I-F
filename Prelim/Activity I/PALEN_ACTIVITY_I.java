a4ea547021db4e6fdd28c9f807dd7f83e28f2478
Herzel Palen <palenherzel05@gmail.com>
2024-10-16 12:27:58 +0800

    Add files via upload

---
+++ Prelim/Activity I/PALEN_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class PALEN_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Herzel Jean Palen");
+        System.out.println(19);
+        System.out.println(20);
+        System.out.println(19+20);
+    }   
+}
