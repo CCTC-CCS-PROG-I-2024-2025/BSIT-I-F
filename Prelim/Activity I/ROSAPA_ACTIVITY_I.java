e4bb3558d0c572cec0e2a8d026d3922511221be0
Cherry Mae Rosapa <cherrymaerosapa3@gmail.com>
2024-10-16 12:29:45 +0800

    Add files via upload

---
+++ Prelim/Activity I/ROSAPA_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class ROSAPA_ACTIVITY_I {
+  public static void main(String[] args) {
+    System.out.println("Cherry Mae Rosapa");
+    System.out.println(20);
+    System.out.println(20);
+    System.out.println(20 + 20);
+  }
+}
