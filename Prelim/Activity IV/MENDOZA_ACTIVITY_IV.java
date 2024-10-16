326e8cfd177d1cef4ad618e0e7ba339c09302386
Charisse Mae Mendoza <charissemendoza194@gmail.com>
2024-10-16 12:33:49 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MENDOZA_ACTIVITY_IV.java
@@ -0,0 +1,19 @@
+
+class MENDOZA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int  a = 20;
+        int  b = 10;
+        int  c = 5;
+
+        int  sum = a+b;
+        int  diff = a-c;
+        int prod = b*c;
+        int quot = a/c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+    }
+}
