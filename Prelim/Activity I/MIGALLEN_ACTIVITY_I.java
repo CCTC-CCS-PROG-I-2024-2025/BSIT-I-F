3253c6e3ab49b43f1608132f90586ee6191f6625
Feil Migallen <migallenfeil005@gmail.com>
2024-10-16 12:28:35 +0800

    Add files via upload

---
+++ Prelim/Activity I/MIGALLEN_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+class MIGALLEN_ACTIVITY_I {
+     public static void main(String[] args) {
+         System.out.println("FEIL M. MIGALLEN");
+         System.out.println(18);
+         System.out.println(23);
+         System.out.println(18 + 23);
+
+         int age = 18;
+
+         if(age <= 20) {
+            System.out.println("Christian Pogi");
+         } else if(age = 18) {
+            System.out.println("Christian Pasikat");
+         } else if(age <= 25) {
+            System.out.println("Christian Buntisan Mo Ako");
+         }
+
+        
+         
+         
+        
+
+     }
+}    
