6f91951b82445156358194ced3afa946a50371e8
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2024-10-16 12:39:53 +0800

    Add files via upload

--- Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
+++ Prelim/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,25 +1,12 @@
-0.
-import java.util.Scanner;
-
 class HERALDEZ_ACTIVITY_I{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-            System.out.println("Color:");
-            String color = sc.nextLine();
-
-            switch (color) {
-              case "red": 
-                System.out.println("Box #1");
-                break;
-              case "green": 
-                System.out.println("Box #2");
-                break;
-              case "black":
-                System.out.println("Box #3");
-                break;     
-            }
+    public static void main (String[] args) {
+        //Name
+        System.out.println("KURT JOHN HERALDEZ");
+        //My age
+        System.out.println(20);
+        //Seatmate's age
+        System.out.println(18);
+        //Sum of our age
+        System.out.println(20+18);
     }
 }
-
-    
-
