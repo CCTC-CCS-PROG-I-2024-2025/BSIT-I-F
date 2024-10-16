87a3b3dc86ea95e42de6840a4cc0d2596cbdb491
amoraelyse02 <luisenatasha02@gmail.com>
2024-10-16 12:36:07 +0800

    Add files via upload

---
+++ Prelim/Activity I/ARANAS_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+class ARANAS_ACTIVITY_I {
+    public static void main (String[] args) {
+        System.out.println("Stephane Aranas");
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20+20);
+
+    }
+}
