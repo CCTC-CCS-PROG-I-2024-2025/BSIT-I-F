4fb163c1149e48e859dec2dd62e228875eee2b02
Ivan Larubis <Ivanlarubis@gmail.com>
2024-10-30 12:10:58 +0800

    Add files via upload

---
+++ Prelim/Activity I/LARUBIS_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+class LARUBIS_ACTIVITY_I { 
+    
+    public static void main(String[] args) {
+            System.out.println("Ivan A. Larubis");
+            System.out.println(20);
+            System.out.println(18);
+            System.out.println(20+18 );
+        }
+    }
