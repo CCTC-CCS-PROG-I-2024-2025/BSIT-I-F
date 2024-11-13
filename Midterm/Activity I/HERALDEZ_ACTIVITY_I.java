f2dcd81411661a25f74df2806ce1a1237629d227
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2024-11-13 13:33:53 +0800

    Add files via upload

---
+++ Midterm/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class HERALDEZ_ACTIVITY_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Color: ");
+            String color = sc.nextLine();
+
+                   switch (color) {
+                   case "red":
+                       System.out.println(1);
+                       break;
+                   case "green":
+                       System.out.println(2);
+                       break;
+                   case "black":
+                       System.out.println(3);
+                       break;
+                   }
+    }
+}
