919695d3d364ed39a0cf7575f31a3867beb251f1
Johannes Michael Mejia <ggxaragares@gmail.com>
2024-11-14 15:38:37 +0800

    Add files via upload

---
+++ Midterm/Activity IV/MEJIA_ACTIVITY_IV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MEJIA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a non-negative integer:");
+            int number = sc.nextInt();
+
+            if (number < 0) {
+                System.out.println("Please enter a non-negative integer.");
+            } else {
+
+                int fact = 1;
+
+                for (int i = 1; i <= number; i++) {
+                    fact *= i;
+                    System.out.println(fact);
+                }
+            }
+        }
+    }
+}
