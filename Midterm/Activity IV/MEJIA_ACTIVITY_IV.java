ecd593d142514b0ec0b0cf7f18ca0da2ec6be504
Johannes Michael Mejia <ggxaragares@gmail.com>
2024-11-14 15:45:08 +0800

    Update MEJIA_ACTIVITY_IV.java

--- Midterm/Activity IV/MEJIA_ACTIVITY_IV.java
+++ Midterm/Activity IV/MEJIA_ACTIVITY_IV.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class MEJIA_ACTIVITY_IV {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter a non-negative integer:");
-            int number = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer:");
+        int number = sc.nextInt();
 
-            if (number < 0) {
-                System.out.println("Not a non-negative integer.");
-            } else {
+        if (number < 0) {
+            System.out.println("Not a non-negative integer.");
+        } else {
 
-                int fact = 1;
+            int fact = 1;
 
-                for (int i = 1; i <= number; i++) {
-                    fact *= i;
-                    System.out.println(fact);
-                }
+            for (int i = 1; i <= number; i++) {
+                fact *= i;
+                System.out.println(fact);
             }
         }
     }
