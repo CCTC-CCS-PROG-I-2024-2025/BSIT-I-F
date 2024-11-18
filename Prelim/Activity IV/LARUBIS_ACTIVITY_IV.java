ad6e63de8be1ccabb3dcbe776935e81f5e9cc344
Ivan Larubis <Ivanlarubis@gmail.com>
2024-11-18 14:44:26 +0800

    Add files via upload

---
+++ Prelim/Activity IV/LARUBIS_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+class LARUBIS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int  a = 22;
+        int  b = 12;
+        int  c = 5;
+
+        int  sum = a+b;
+        int  diff = a-c;
+        int prod = b*c;
+        int quot = a/c;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+    }
+}
