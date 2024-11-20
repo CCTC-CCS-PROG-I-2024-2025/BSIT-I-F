083b990bfa0d5e957b9a9652d5562813f386e71b
Tristan Dreco Garcia <garciatristan66@gmail.com>
2024-11-20 12:21:48 +0800

    Add files via upload

---
+++ Midterm/Activity I/GARCIA_ACTIVITY_I.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class GARCIA_ACTIVITY_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("color");
+        String color = sc.nextLine();
+
+        switch (color){
+            case "red":
+               System.out.println(1);
+               break;
+            case "green":
+               System.out.println(2);
+               break;  
+            case "black":
+               System.out.println(3);
+               break;    
+        }
+        
+
+
+    }
+}
