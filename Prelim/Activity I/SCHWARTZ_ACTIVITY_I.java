b890a5e1e15091a679e4c526752ca6d6d39819f4
Dominque Louis D. Schwartz <dolinomenick@gmail.com>
2024-12-11 11:17:54 +0800

    Update SCHWARTZ_ACTIVITY_I.java

--- Prelim/Activity I/SCHWARTZ_ACTIVITY_I.java
+++ Prelim/Activity I/SCHWARTZ_ACTIVITY_I.java
@@ -4,7 +4,7 @@
 public class SCHWARTZ_ACTIVITY_I {
 
     public static void main(String[] args) {
-        System.out.println(DOMINIQUE LOUIS SCHWARTZ);
+        System.out.println("DOMINIQUE LOUIS SCHWARTZ");
         System.out.println("23");
         System.out.println("20");
         System.out.println("43");
