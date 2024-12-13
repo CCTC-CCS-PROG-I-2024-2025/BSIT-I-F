e44208e6a0afe7c0a25e9d242f07a3c787c8448f
Ana Lea Escorial <analeatangoan08@gmail.com>
2024-12-13 13:44:15 +0800

    Add files via upload

---
+++ Midterm/Activity I/ESCORIAL_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ESCORIAL_ACTIVITY_I {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("color");
+    String color = sc.nextLine();
+
+    switch(color) {
+        case "red":
+          System.out.println("box #1");
+          break;
+        case"green":
+          System.out.println("box #2");
+          break;
+        case "black":
+          System.out.println("box #3");
+          break;
+    
+        }
+    }
+}
+
