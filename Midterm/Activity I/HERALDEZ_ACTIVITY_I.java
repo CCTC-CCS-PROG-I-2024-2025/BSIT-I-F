dc120b0ac1a3fb76c45e557863318299ff85634f
Melli Aileen D. Heraldez <heraldezmelli7@gmail.com>
2024-12-13 15:23:27 +0800

    Add files via upload

--- Midterm/Activity I/HERALDEZ_ACTIVITY_I.java
+++ Midterm/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-class HERALDEZ_ACTIVITY_1 {
+
+class HERALDEZ_ACTIVITY_I {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-            System.out.println("Color: ");
-            String color = sc.nextLine();
-                   switch (color) {
-                   case "red":
-                       System.out.println(1);
-                       break;
-                   case "green":
-                       System.out.println(2);
-                       break;
-                   case "black":
-                       System.out.println(3);
-                       break;
-                   }
+    Scanner sc = new Scanner (System.in);
+    System.out.println("color");
+    String color = sc.nextLine();
+
+    switch(color) {
+        case "red":
+          System.out.println("box #1");
+          break;
+        case"green":
+          System.out.println("box #2");
+          break;
+        case "black":
+          System.out.println("box #3");
+          break;
+    
+        }
     }
 }
