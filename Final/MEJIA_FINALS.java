e5fe6e83265bd7d2ff05410930ca6e459a489874
Johannes Michael Mejia <ggxaragares@gmail.com>
2024-12-13 23:30:10 +0800

    Update MEJIA_FINAL.java

--- Final/MEJIA_FINALS.java
+++ Final/MEJIA_FINALS.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MEJIA_FINALS {
+public class MEJIA_FINAL {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double totBill = 0;
