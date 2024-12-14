fcf3bd99075170afed0c6049ebd2ddda6461dbb1
andreejohn28 <andreejohndecalan28@gmail.com>
2024-12-14 10:13:49 +0800

    Add files via upload

---
+++ Prelim/Activity I/DECALAN_ACTIVITY_l.java
@@ -0,0 +1,10 @@
+class DECALAN_ACTIVITY_I {
+    public static void main(String[] args){
+          System.out.println("Jessa Mae R. Dadula");
+          System.out.println(18);
+          System.out.println(18);
+          System.out.println(18+18);
+    }
+}
+
+    
