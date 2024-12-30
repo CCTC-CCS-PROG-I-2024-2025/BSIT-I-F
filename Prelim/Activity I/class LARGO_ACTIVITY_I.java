d41c527bafe80c4b5b8b5f766ada929d1b566b27
Largo,Timothy John O. <hoovyrickamaroni123@gmail.com>
2024-12-30 11:34:54 +0800

    Add files via upload

---
+++ Prelim/Activity I/class LARGO_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class LARGO_ACTIVITY_I{
+    public static void main(String[] args) {
+      System.out.println("Jacob Isaac O. Largo");
+      System.out.println(20);
+      System.out.println(22);
+      System.out.println(20+22);
+    }
+}
