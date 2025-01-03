8f2096d1aee4fa9cdf1cbf4fe3dbc38a5160f65f
Johnrepaso <repasojohn166@gmail.com>
2025-01-02 23:18:27 -0800

    Add files via upload

---
+++ Prelim/Activity IV/REPASO_ACTIVITY_IV.java
@@ -0,0 +1,21 @@
+public class REPASO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        int age = 60;
+        int number = 55;
+        int price = 25;
+        int result = age + number;
+        int amount = number - price;
+        int total = price * number;
+        int height = number / price;
+        System.out.println(result);
+        System.out.println(amount);
+        System.out.println(total);
+        System.out.println(height);
+
+
+
+
+        
+
+    }
+}
