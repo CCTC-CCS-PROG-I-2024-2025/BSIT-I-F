5625b781c8160dc4a9768c57f321649dc3011239
Tristan Dreco Garcia <garciatristan66@gmail.com>
2025-01-03 13:18:55 +0800

    Add files via upload

---
+++ Midterm/Activity I/CABARDO_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class CABARDO_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the color:");
+        String color = sc.nextLine();
+
+        switch (color) {
+            case "red":
+                System.out.println(1);
+                break;
+            case "green":
+                System.out.println(2);
+                break;
+            case "black":
+                System.out.println(3);
+                break;
+
+        }
+
+    }
+
+}
