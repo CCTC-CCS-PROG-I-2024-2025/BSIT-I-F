8243854354dc188f0a35a8c8b935a319637b9255
Johnrepaso <repasojohn166@gmail.com>
2025-01-02 23:18:09 -0800

    Add files via upload

---
+++ Prelim/Activity I/REPASO_ACTIVITY_I.java
@@ -0,0 +1,9 @@
+public class REPASO_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("JOHN WEVIR REPASO");
+        System.out.println(18);
+        System.out.println(18);
+        System.out.println(18+18);
+
+    }
+}
