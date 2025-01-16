ec5c7c58e327719b53d9badc90d04560974ed471
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2025-01-16 16:17:39 +0800

    Add files via upload

--- PROG II/Prelim/Activities/Activity I/HERALDEZ_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
-class HERALDEZ_ACTIVITY_I {
-   public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         String[] names = new String[5];
-         System.out.println("Enter 5 names:");
-         
-         for (int i = 0; i < names.length; i++) {
-            System.out.print("Name " + (i + 1) + ":");
-            names[i] = scanner.nextLine();
-         }
-         System.out.println("\nchoose a number (1 to 5) to display the corresponding name");
-         int index = scanner.nextInt();
+public class HERALDEZ_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[5];
 
-         if (index >= 1 && index <= 5) {
-            System.out.println("Selected name: " + names[index - 1]);
-         } else {
-            System.out.println("Invalid number. Please choose between 1 and 5.");
-         }     
-   }
+        System.out.println("Enter 5 names: ");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println("Choose a number (0 to 4) to display the corresponding name: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index <= 4) {
+            System.out.println("Selected name: " + names[index]);
+        } else {
+            System.out.println("Invalid number! Please choose from 0 to 4!");
+        }
+    }
 }
