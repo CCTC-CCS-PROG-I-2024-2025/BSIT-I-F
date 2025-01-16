d9efe0bbb61716b6ad09798a8fb93184288e594e
Marily Niere <marilynniere18@gmail.com>
2025-01-16 16:10:06 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/NIERE_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class NIERE_ACTIVITY_I {
+   public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         String[] names = new String[5];
+         System.out.println("Enter 5 names:");
+         
+         for (int i = 0; i < names.length; i++) {
+            System.out.print("Name " + (i + 1) + ":");
+            names[i] = scanner.nextLine();
+         }
+         System.out.println("\nchoose a number (1 to 5) to display the corresponding name");
+         int index = scanner.nextInt();
+
+         if (index >= 1 && index <= 5) {
+            System.out.println("Selected name: " + names[index - 1]);
+         } else {
+            System.out.println("Invalid number. Please choose between 1 and 5.");
+         }     
+   }
+}
