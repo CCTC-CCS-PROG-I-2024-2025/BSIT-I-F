45a2e2d76a577a63d1aaeb417130f75f11a46e90
Charisse Mae Mendoza <charissemendoza194@gmail.com>
2025-01-17 11:52:19 +0800

    Update MENDOZA_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/MENDOZA_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/MENDOZA_ACTIVITY_I.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 class MENDOZA_ACTIVITY_I {
-   public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         String[] names = new String[5];
-         System.out.println("Enter 5 names:");
-         
-         for (int i = 0; i < names.length; i++) {
-            System.out.print("Name " + (i + 1) + ":");
-            names[i] = scanner.nextLine();
-         }
-         System.out.println("\nchoose a number (1 to 5) to display the corresponding name");
-         int index = scanner.nextInt();
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        Scanner sc = new Scanner(System.in);
 
-         if (index >= 1 && index <= 5) {
-            System.out.println("Selected name: " + names[index - 1]);
-         } else {
-            System.out.println("Invalid number. Please choose between 1 and 5.");
-         }     
-   }
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.print("Enter a number (0-4) to display the corresponding name: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index < names.length) {
+            System.out.println("The name selected is: " + names[index]);
+        } else {
+            System.out.println("Invalid number. Please enter a number between 0 and 4.");
+        }
+    }
 }
