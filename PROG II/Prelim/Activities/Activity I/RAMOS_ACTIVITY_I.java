c7a4b89fc1d9decaeb17fe1d3314e4fa8b928961
Minnie Grace D. Ramos <ramosminniegrace@gmail.com>
2025-01-28 10:46:34 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/RAMOS_ACTIVITY_I.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RAMOS_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] names = new String[5];
+
+        System.out.println("Please enter 5 names:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+
+        if (number >= 1 && number <= 5) {
+            System.out.println(names[number - 1]);
+        } else {
+            System.out.println("Only 1 to 5");
+        }
+    }
+}
