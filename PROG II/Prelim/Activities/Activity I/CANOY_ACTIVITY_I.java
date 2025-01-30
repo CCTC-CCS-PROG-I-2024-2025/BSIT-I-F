aea79d76f329a891b5eddd6b05f655b66ef735b8
AHRONCANOY1 <ahronnisas26@gmail.com>
2025-01-30 14:10:59 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/CANOY_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CANOY_ACTIVITY_I {
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.print("Enter a number (0-4) to display the corresponding name: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index < names.length) {
+            System.out.println("The name selected is: " + names[index]);
+        } else {
+            System.out.println("Invalid number. Please enter a number between 0 and 4.");
+        }
+    }
+}
