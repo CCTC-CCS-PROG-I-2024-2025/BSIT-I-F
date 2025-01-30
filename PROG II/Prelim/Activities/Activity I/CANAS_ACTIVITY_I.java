96121592e1f514377c414c33755ccb97dfc7adde
Neil Brian Cañas <canasbrian01@gmail.com>
2025-01-30 13:52:36 +0800

    Update CANAS_ACTIVITY_I.java

--- PROG II/Prelim/Activities/Activity I/CANAS_ACTIVITY_I.java
+++ PROG II/Prelim/Activities/Activity I/CANAS_ACTIVITY_I.java
@@ -1,47 +1,22 @@
 import java.util.Scanner;
-public class CANAS_ACTIVITY_I{
+
+class CANAS_ACTIVITY_I {
     public static void main(String[] args) {
         String[] names = new String[5];
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Name 1:");
-        String name1 = input.nextLine();
-        System.out.println("Enter Name 2:");
-        String name2 = input.nextLine();
-        System.out.println("Enter Name 3:");
-        String name3 = input.nextLine();
-        System.out.println("Enter Name 4:");
-        String name4 = input.nextLine();
-        System.out.println("Enter Name 5:");
-        String name5 = input.nextLine();
-        names[0] = name1;
-        names[1] = name2;
-        names[2] = name3;
-        names[3] = name4;
-        names[4] = name5; 
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("What name you want to display: ");
-        int choice = input.nextInt();
-
-        switch (choice){
-            case 1:
-                System.out.println(names[0]);
-                break;
-            case 2:
-                System.out.println(names[1]);
-                break;
-            case 3:
-                System.out.println(names[2]);
-                break;
-            case 4:
-                System.out.println(names[3]);
-                break;
-            case 5:
-                System.out.println(names[4]);
-                break;
-           
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
         }
 
+        System.out.print("Enter a number (0-4) to display the corresponding name: ");
+        int index = sc.nextInt();
 
-
+        if (index >= 0 && index < names.length) {
+            System.out.println("The name selected is: " + names[index]);
+        } else {
+            System.out.println("Invalid number. Please enter a number between 0 and 4.");
+        }
     }
 }
