efa2d08b3f2e6dce754c3c696b6b43cdd425c119
amoraelyse02 <luisenatasha02@gmail.com>
2025-01-30 13:44:41 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/ARANAS_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ARANAS_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[5];
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+        System.out.println("\nChoose a number (1 to 5) to display the corresponding name:");
+        int index = sc.nextInt();
+
+        if (index >= 1 && index <= 5) {
+            System.out.println("Selected name: " + names[index - 1]);
+        } else {
+            System.out.println("Invalid number. Please choose between 1 and 5.");
+        }
+    }
+}
