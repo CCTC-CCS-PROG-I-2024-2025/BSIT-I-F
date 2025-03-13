44bda29a9febb33dd68e3337c0a166f3a8b3dbdc
Jhon Niño F. Cañalita <jncanalita2004@gmail.com>
2025-03-13 13:51:16 +0800

    Add files via upload

---
+++ Prelim/Activity IV/public class CANALITAJ_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+    public class CANALITAJ_ACTIVITY_IV {
+        public static void main(String[] args) {
+    
+            int x = 15;
+            int y = 10;
+            int c = 9;
+    
+            int result = x + y;
+            int result1 = y - x;
+            int result2 = x / c;
+            int result3 = c * y;
+    
+            System.out.println(result);
+            System.out.println(result1);
+            System.out.println(result2);
+            System.out.println(result3);
+                }
+    }
