7dceaab0d5c43cf11a8ba76a99e15c3d9650a58b
Byrhon P. Castolo <castbyrhon@gmail.com>
2025-03-13 13:48:00 +0800

    Add files via upload

---
+++ Midterm/Activity I/PALEN_ACTIVITY_I.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class PALEN_ACTIVITY_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+       
+        System.out.println("Enter the color");
+        String color = sc.nextLine();
+        
+
+        switch (color) {
+            case "black":
+            System.out.println(3);
+            break;
+
+            case "green":
+            System.out.println(2);
+            break;
+
+            case "red":
+            System.out.println(1);
+            break;
+        }
+        
+
+        }
+}
