46b68f04daa39f359102a3500851450dfb62b871
Kurt John D. Heraldez <kurtjohnheraldez@gmail.com>
2025-03-13 13:43:53 +0800

    Update HERALDEZ_ACTIVITY_I.java

--- Midterm/Activity I/HERALDEZ_ACTIVITY_I.java
+++ Midterm/Activity I/HERALDEZ_ACTIVITY_I.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class HERALDEZ_ACTIVITY_1 {
+class HERALDEZ_ACTIVITY_I {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             System.out.println("Color: ");
