8fa5a073ff44c8197f558b25b98df860874554ea
Neil Brian Cañas <canasbrian01@gmail.com>
2025-03-13 18:50:35 +0800

    Create CAÑAS_ACTIVITY_I.java

---
+++ Prelim/Activity I/CAÑAS_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class CAÑAS_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("NEIL BRIAN CAÑAS");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18+19);
+    }
+}
