17e17b7d29e0ed7129fc823423fccc245f73e734
Christian M. Ubas <bewakerchristian@gmail.com>
2025-03-13 13:46:10 +0800

    UBAS_ACTIVITY_ACT1

--- Midterm/Activity I/UBAS_ACTIVITY_MIDTERM_PROG1.java
+++ Midterm/Activity I/UBAS_ACTIVITY_MIDTERM_PROG1.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
-class UBAS__ACTIVITY_IV {
+class UBAS_ACTIVITY_I {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a non-negative integer: ");
-        int number = sc.nextInt();
-           
-            int factorial = 1;
+        System.out.println("Enter the color:");
+        String color = sc.nextLine();
 
-            for (int i = 1; i <= number; i++) {
-                factorial *= i; 
-            }
-            System.out.println("The factorial of " + number + " is " + factorial);
+        switch (color) {
+            case "red":
+                System.out.println(1);
+                break;
+            case "green":
+                System.out.println(2);
+                break;
+            case "black":
+                System.out.println(3);
+                break;
+
+        }
+
     }
+
 }
-
-
-    
-
-
-
-    
-
-
-        
