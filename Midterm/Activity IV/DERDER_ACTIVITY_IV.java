719f83454254a10439521c694f1174f092ec0af3
Izza May Derder <izamd4710@gmail.com>
2025-03-13 13:55:20 +0800

    Add files via upload

---
+++ Midterm/Activity IV/DERDER_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DERDER_ACTIVITY_IV {
+  public static void main(String[]args){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter a non-negative integer");
+    int number = sc.nextInt();
+
+    if (number<0){
+      System.out.println("Enter a non-negative integer");
+    }else{
+
+         int factorial = 1;
+         
+         for (int i = 1;i<= number;i++){
+             factorial*=i;
+             System.out.println("The factorialof"+number+"is:"+ factorial);
+         }
+         
+        
+    }
+    
+  }    
+}
