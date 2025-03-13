ab6f68f0ed76a1ceeadfdf99550cff6193354e64
angelotoquero <angelotoquero09@gmail.com>
2025-03-13 19:05:04 +0800

    Create TOQUERO_ACTIVITY_I.java

---
+++ Prelim/Activity I/TOQUERO_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class TOQUERO_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("Angelo Toquero");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18+19);
+    }
+}
