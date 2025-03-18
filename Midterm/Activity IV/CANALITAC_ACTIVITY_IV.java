1d3bd527b0f7ff8cd5d3ef27de47b944460d2c07
carlitojrfajardo <carlitojrfajardo@gmail.com>
2025-03-18 19:12:43 +0800

    Add files via upload

---
+++ Midterm/Activity IV/CANALITAC_ACTIVITY_IV.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    class CANALITAC_ACTIVITY_IV{
+        public static void main (String[] args){
+            Scanner sc= new Scanner (System.in);
+            System.out.println("enter a non-negative integer");
+            int number= sc.nextInt();
+            
+            int factorial=1;
+
+            for (int i=1;i<=number;i++){
+                factorial*=i;
+            }
+            System.out.println("Factorial of"+ number + "is" + factorial);
+        }
+    }
