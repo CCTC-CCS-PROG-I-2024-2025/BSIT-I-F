e6472e14206bd6c65f43b0a0fbebad07ed73bdef
AHRONCANOY1 <ahronnisas26@gmail.com>
2025-03-19 15:04:57 +0800

    Create CANOY_ACTIVITY_I.java

---
+++ Prelim/Activity I/CANOY_ACTIVITY_I.java
@@ -0,0 +1,8 @@
+class CANOY_ACTIVITY_I {
+    public static void main(String[] args) {
+        System.out.println("AHRON CANOY");
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(18+19);
+    }
+}
