c4dcb702ebef613622c8fb216674120105c2a81d
Stephane Arañas <luisenatasha02@gmail.com>
2025-03-19 15:34:04 +0800

    Add files via upload

---
+++ Midterm/Activity I/ARANAS_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+    
+    class ARANAS_ACTIVITY_I {
+        public static void main(String [] args){
+            Scanner sc = new Scanner (System.in);
+            System.out.println("Color");
+            String color = sc.nextLine ();
+
+            switch (color){
+            case "red":
+                System.out.println("box #1");
+                break;
+            case "green":
+                System.out.println("box #2");
+                break;
+            case "black":
+                System.out.println("box #3");
+                break;
+                
+            }
+        }
+    }
