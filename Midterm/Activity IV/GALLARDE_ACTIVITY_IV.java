b5360efd96601f5ebef55e999f8fd9cc77d8e6a2
Debbie Mariz Gilhang <marizgilhang104@gmail.com>
2025-03-19 22:27:21 +0800

    Add files via upload

---
+++ Midterm/Activity IV/GALLARDE_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GALLARDE_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a non-negative integer: ");
+        int number = sc.nextInt();
+
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println("The factorial of " + number + " is " + factorial);
+    }
+}
